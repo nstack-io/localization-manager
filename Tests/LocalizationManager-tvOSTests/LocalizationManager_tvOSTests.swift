ed973ed2ca8791de56c7449c155729eb90c271a5
Andrew Lloyd <anll@nodesagency.com>
2019-08-27 11:59:07 +0100

    refactor translation to localization for main project

---
+++ Tests/LocalizationManager-tvOSTests/LocalizationManager_tvOSTests.swift
@@ -0,0 +1,21 @@
+//
+//  LocalizationManager_tvOSTests.swift
+//  LocalizationManager-tvOSTests
+//
+//  Created by Peter Bødskov on 20/08/2019.
+//  Copyright © 2019 Nodes. All rights reserved.
+//
+
+import XCTest
+@testable import LocalizationManager_tvOS
+
+class LocalizationManagerTVOSTests: SharedLocalizationManagerTests {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+}
