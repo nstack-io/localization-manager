5d23555750772bbc996c5cea286b79e9a91f3686
Chris Combs <chco@nodes.dk>
2020-03-25 14:46:00 +0100

    Added source path

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "LocalizationManager"
+            name: "LocalizationManager",
+            path: "LocalizationManager/Classes"
         )
     ]
 )
