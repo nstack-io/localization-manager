11d214b0e79f758261986a9ddef3823d48a8b9f4
Chris Combs <chco@nodes.dk>
2020-03-25 14:35:47 +0100

    Add package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "LocalizationManager",
+    platforms: [.iOS(.v10), .macOS(.v10_12), .tvOS(.v10), .watchOS(.v3)],
+    products: [
+        .library(name: "LocalizationManager", targets: ["LocalizationManager"])
+    ],
+    targets: [
+        .target(
+            name: "LocalizationManager"
+        )
+    ]
+)
